a3572feb613640cd9fdf54e152cd51ecf32bec07
DongHeon Lee <niche3600@gmail.com>
2022-03-28 09:45:42 +0900

    use List example

---
+++ a1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+namespace a1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            a.Add(3);
+            a.Add(2);
+            a.Add(1);
+            a.Insert(1, 4);
+
+            int index = -1;
+            foreach (int i in a) {
+                index++;
+                Console.WriteLine($"a[{index}]={i}");
+            }
+        }
+    }
+}
