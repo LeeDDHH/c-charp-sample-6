1bc9590c22d4a20c422c059b0cc9772777fb605c
DongHeon Lee <niche3600@gmail.com>
2022-03-28 12:11:58 +0900

    delegate example

---
+++ a5/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace a5
+{
+    delegate void Operation(int a, int b);
+
+    class Calc
+    {
+        public void Sub(int a, int b)
+        {
+            Console.WriteLine($"{a} - {b} = {a - b}");
+        }
+    }
+    class Program
+    {
+
+        static void Add(int a, int b)
+        {
+            Console.WriteLine($"{a} + {b} = {a + b}");
+        }
+
+        static void Main(string[] args)
+        {
+            Calc c = new Calc();
+
+            // デリゲートの設定
+            Operation o1 = new Operation(Add);
+            Operation o2 = new Operation(c.Sub);
+
+            // デリゲートで設定したメソッドの呼び出し
+            o1(2, 1);
+            o2(2, 1);
+        }
+    }
+}
