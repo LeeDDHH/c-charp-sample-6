e7ed8655c78f45cb1d9d037e3e462711d34eb362
DongHeon Lee <niche3600@gmail.com>
2022-03-28 12:18:49 +0900

    use Multiple delegate example

---
+++ a6/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace a6
+{
+    class Program
+    {
+
+        // デリゲートの宣言
+        delegate void Action(int a);
+
+        static void Func1(int a)
+        {
+            Console.WriteLine($"a={a}");
+        }
+
+        static void Func2(int a)
+        {
+            Console.WriteLine($"a*2={a*2}");
+        }
+
+        static void Func3(int a)
+        {
+            Console.WriteLine($"a*3={a*3}");
+        }
+
+        static void Main(string[] args)
+        {
+            Action a = new Action(Func1);
+            a += new Action(Func2);
+            a += new Action(Func3);
+            a(3);
+        }
+    }
+}
