0f2191d31745252ae97377fb454cbdf1a1df7d29
DongHeon Lee <niche3600@gmail.com>
2022-03-28 11:05:10 +0900

    List type Remove method example

---
+++ a2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace a2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> a = new List<String>();
+
+            a.Add("Taro");
+            a.Add("Hanako");
+            a.Add("Jiro");
+            a.Add("Kaoru");
+
+            a.Remove("Taro");
+            a.RemoveAt(1);
+
+            foreach (String s in a)
+            {
+                Console.WriteLine($"{s}");
+            }
+        }
+    }
+}
