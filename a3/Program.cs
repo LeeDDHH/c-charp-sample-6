cb983cda3bdb132e80666bb8d8a1af8d9a154ab2
DongHeon Lee <niche3600@gmail.com>
2022-03-28 11:17:40 +0900

    use DIctionary example

---
+++ a3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace a3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 連想記憶クラスの生成
+            Dictionary<string, string> capital = new Dictionary<string, string>();
+
+            // データの追加
+            capital["日本"] = "東京";
+            capital["イギリス"] = "ロンドン";
+            capital["フランス"] = "パリ";
+            capital["中国"] = "北京";
+            Console.WriteLine($"世界の首都");
+
+            foreach (String s in capital.Keys)
+            {
+                Console.WriteLine($"{s}の首都は{capital[s]}");
+            }
+        }
+    }
+}
