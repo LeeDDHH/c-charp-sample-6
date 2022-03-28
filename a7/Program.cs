f2c02a0ceebb7635f6976e7aa344bac5c47f1146
DongHeon Lee <niche3600@gmail.com>
2022-03-28 12:31:03 +0900

    try catch finally example

---
+++ a7/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace a7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    int a = GetNum(i);
+                    int b = 5;
+                    Console.Write($"{a} / {b} = ");
+                    Console.WriteLine($"{Calc(a,b)}");
+                }
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine($"");
+                Console.WriteLine($"0による割り算発生");
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"配列の範囲外にアクセスしました");
+            }
+            finally
+            {
+                Console.WriteLine($"終了");
+            }
+        }
+
+        private static int Calc(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int GetNum(int index)
+        {
+            int[] num = { 1, 2, 3, 4 };
+            return num[index];
+        }
+    }
+}
