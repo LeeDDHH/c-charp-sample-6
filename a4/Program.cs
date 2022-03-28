76e1b532eeb7dfc84c58598ad7236ace0c4a26c9
DongHeon Lee <niche3600@gmail.com>
2022-03-28 11:34:30 +0900

    use HashSet example

---
+++ a4/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace a4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> t = new HashSet<int>();
+
+            t.Add(1);
+            t.Add(2);
+            t.Add(3);
+            t.Add(1);
+
+            foreach (int i in t)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
